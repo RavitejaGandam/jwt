8250bff087a999f805139fc8b98c7163e9a52765
Raviteja Gandam <gandamraviteja@gmail.com>
2024-06-08 11:26:13 +0530

    done

--- src/main/java/com/grt/jwt/HelloResource.java
+++ src/main/java/com/grt/jwt/HelloResource.java
@@ -1,7 +1,12 @@
 package com.grt.jwt;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloResource {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello ravi";
+    }
 }
--- src/main/java/com/grt/jwt/SecurityConfigurer.java
+++ src/main/java/com/grt/jwt/SecurityConfigurer.java
@@ -1,4 +1,11 @@
 package com.grt.jwt;
 
-public class SecurityConfigurer {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer  {
 }
